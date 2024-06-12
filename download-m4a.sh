c2a4b2da20011d1c00134a411920564323d2bc14
Valentin Panchenko <senior.konung@gmail.com>
2024-06-12 14:05:13 +0300

    gpg signature test

--- download-m4a.sh
+++ download-m4a.sh
@@ -4,22 +4,22 @@ IMAGE_NAME="youtube-dl"
 DIR_NAME="files"
 
 if [ "$USER" != "root" ]; then
-	echo "Для выполнения скрипта нужны права sudo!"
-	exit 0
+    echo "Для выполнения скрипта нужны права sudo!"
+    exit 0
 fi
 
 if ! [ -d ./$DIR_NAME ]; then
-	mkdir $DIR_NAME
+    mkdir $DIR_NAME
 fi
 
 if [ ! "$(docker image ls -q -f before=$IMAGE_NAME)" ]; then
-	echo "Создание Docker образа..."
-	docker build -t $IMAGE_NAME $PWD
+    echo "Создание Docker образа..."
+    docker build -t $IMAGE_NAME $PWD
 fi
 
 for url in $@ 
 do
-	docker run --rm -v $PWD/files:/home/files -w /home/files $IMAGE_NAME youtube-dl $url -x --audio-format m4a
+    docker run --rm -v $PWD/files:/home/files -w /home/files $IMAGE_NAME youtube-dl $url -x --audio-format m4a
 done
 
 chown seniorkonung:seniorkonung -R $PWD/files
